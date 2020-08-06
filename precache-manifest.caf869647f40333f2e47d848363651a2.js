63323abd57188288b8ae1e7545c742aa25c7f5c5
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2020-08-06 10:36:52 +0300

    Updates

---
+++ precache-manifest.caf869647f40333f2e47d848363651a2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7e9f9ede953f6f5bb833fd8f34f425",
+    "url": "/music-player-proto/index.html"
+  },
+  {
+    "revision": "fef8255c0e8ae2401b22",
+    "url": "/music-player-proto/static/css/main.0a222da1.chunk.css"
+  },
+  {
+    "revision": "cb1a3e4662882df02e8c",
+    "url": "/music-player-proto/static/js/2.916aa980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music-player-proto/static/js/2.916aa980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef8255c0e8ae2401b22",
+    "url": "/music-player-proto/static/js/main.1929777a.chunk.js"
+  },
+  {
+    "revision": "747718238299b14206a6",
+    "url": "/music-player-proto/static/js/runtime-main.e85c1dde.js"
+  },
+  {
+    "revision": "f7c15ffcbba3b96490d5e9000d174d0d",
+    "url": "/music-player-proto/static/media/albumCover.f7c15ffc.jpg"
+  },
+  {
+    "revision": "48e2ffe0e1e10128bd64285ca8351a53",
+    "url": "/music-player-proto/static/media/arrow.48e2ffe0.svg"
+  },
+  {
+    "revision": "24730fb31a1453bf200e92cac70ab41b",
+    "url": "/music-player-proto/static/media/arrowPressed.24730fb3.svg"
+  },
+  {
+    "revision": "322cef78a19126c1be674d6157bb1472",
+    "url": "/music-player-proto/static/media/back.322cef78.svg"
+  },
+  {
+    "revision": "5cb46261e653a4ab8d6880903878f33b",
+    "url": "/music-player-proto/static/media/background.5cb46261.jpg"
+  },
+  {
+    "revision": "00991dd0d27e2c471eb765c729b1b845",
+    "url": "/music-player-proto/static/media/fast-forward-pressed.00991dd0.svg"
+  },
+  {
+    "revision": "d9e57debd6af9948abf1825e50351179",
+    "url": "/music-player-proto/static/media/forward.d9e57deb.svg"
+  },
+  {
+    "revision": "39e478b0d5e5faefe613d1a8c7aa4993",
+    "url": "/music-player-proto/static/media/greyHeart.39e478b0.svg"
+  },
+  {
+    "revision": "4e01da798892736b55361c761406da98",
+    "url": "/music-player-proto/static/media/heart.4e01da79.svg"
+  },
+  {
+    "revision": "4a3a3c57dedd51f0f48d6e971eee7508",
+    "url": "/music-player-proto/static/media/pause.4a3a3c57.svg"
+  },
+  {
+    "revision": "c9c718624a2c3a8a383c26250d62aae7",
+    "url": "/music-player-proto/static/media/play-button.c9c71862.svg"
+  },
+  {
+    "revision": "a37d88c1abeec5c12f082b03eef5f6f2",
+    "url": "/music-player-proto/static/media/pressedBack.a37d88c1.svg"
+  }
+]);
